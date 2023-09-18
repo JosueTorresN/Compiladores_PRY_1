f02a8e9158578bfa31c61b88cfef03149a0d7268
Josue TorresN <jtn1999@estudiantec.cr>
2023-09-17 20:47:28 -0600

    Ya funciona

--- src/main/java/source/Principal.java
+++ src/main/java/source/Principal.java
@@ -4,19 +4,27 @@
  */
 package source;
 
-import java.io.File;
+import jflex.exceptions.SilentExit;
 /**
  *
  * @author jtn19
  */
 public class Principal {
     public static void main(String[] args) {
-        String ruta = System.getProperty("user.dir") + "/src/source/Lexer.flex";
-        generarLexer(ruta);
+//        String ruta = System.getProperty("user.dir") + "/src/source/Lexer.flex";
+        String ruta = "D:\\Universidad\\compiladores\\Compiladores_PRY_1\\src\\main\\java\\source\\Lexer.jflex";
+        System.out.println(ruta);
+        try{
+        String[] archivo = {ruta};
+        jflex.Main.generate(archivo);
+//        generarLexer(ruta);
+        } catch (SilentExit e) {
+            System.out.println(e.getMessage());
+        }
     }
     
-    public static void generarLexer(String ruta) {
-        File archivo = new File(ruta);
+    public static void generarLexer(String ruta) throws SilentExit {
+        String[] archivo = {ruta};
         jflex.Main.generate(archivo);
     }
 }
