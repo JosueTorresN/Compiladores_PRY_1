ee609a493a2245bd9d99929b12738625ea4c23c4
Josue TorresN <jtn1999@estudiantec.cr>
2023-09-18 15:36:57 -0600

    Pipipipiiii

--- src/main/java/source/Principal.java
+++ src/main/java/source/Principal.java
@@ -4,6 +4,9 @@
  */
 package source;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import jflex.exceptions.SilentExit;
 /**
  *
@@ -12,19 +15,27 @@ import jflex.exceptions.SilentExit;
 public class Principal {
     public static void main(String[] args) {
 //        String ruta = System.getProperty("user.dir") + "/src/source/Lexer.flex";
-        String ruta = "D:\\Universidad\\compiladores\\Compiladores_PRY_1\\src\\main\\java\\flex\\Lexer.jflex";
-        System.out.println(ruta);
+        String rutaJFlex = "D:\\Universidad\\compiladores\\Compiladores_PRY_1\\src\\main\\java\\flex\\Lexer.jflex";
+        String rutaCup = "D:\\Universidad\\compiladores\\Compiladores_PRY_1\\src\\main\\java\\flex\\Sintax.cup";
         try{
 //        String[] archivo = {ruta};
 //        jflex.Main.generate(archivo);
-        generarLexer(ruta);
+        generarLexer(rutaJFlex);
+        generateCup(rutaCup);
         } catch (SilentExit e) {
             System.out.println(e.getMessage());
+        } catch (Exception ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
     public static void generarLexer(String ruta) throws SilentExit {
         String[] archivo = {ruta};
         jflex.Main.generate(archivo);
+    }
+    
+    public static void generateCup(String ruta) throws IOException, Exception {
+        String[] archivo = {"-parser", "Sintax", ruta};
+        java_cup.Main.main(archivo);
     }
 }
