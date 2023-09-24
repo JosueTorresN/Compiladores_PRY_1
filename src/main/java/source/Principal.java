6e69fded566e8423e8a1bf62c91f75db2c1f284c
DESKTOP-0SDNMEI\Usuario <anthony00052023@gmail.com>
2023-09-23 23:05:41 -0500

    commit

--- src/main/java/source/Principal.java
+++ src/main/java/source/Principal.java
@@ -4,10 +4,17 @@
  */
 package source;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java_cup.Lexer;
+import java_cup.runtime.Scanner;
+import java_cup.runtime.Symbol;
 import jflex.exceptions.SilentExit;
+
 /**
  *
  * @author jtn19
@@ -15,18 +22,22 @@ import jflex.exceptions.SilentExit;
 public class Principal {
     public static void main(String[] args) {
 //        String ruta = System.getProperty("user.dir") + "/src/source/Lexer.flex";
-        String rutaJFlex = "D:\\Universidad\\compiladores\\Compiladores_PRY_1\\src\\main\\java\\flex\\Lexer.jflex";
-        String rutaCup = "D:\\Universidad\\compiladores\\Compiladores_PRY_1\\src\\main\\java\\flex\\Sintax.cup";
+        String rutaJFlex = "C:/Users/Usuario/Documents/NetBeansProjects/Compiladores_PRY_1/src/main/java/source/Lexer.flex";
+        String rutaCup = "C:/Users/Usuario/Documents/NetBeansProjects/Compiladores_PRY_1/src/main/java/source/Sintax2.cup";
+        //String rutaLexerCup = "C:/Users/Usuario/Documents/NetBeansProjects/Compiladores_PRY_1/src/main/java/source/LexerCup.flex";
         try{
 //        String[] archivo = {ruta};
 //        jflex.Main.generate(archivo);
-        generarLexer(rutaJFlex);
-        generateCup(rutaCup);
-        } catch (SilentExit e) {
-            System.out.println(e.getMessage());
+        //generarLexer(rutaJFlex);
+        generarLexer(rutaLexerCup);//activador//
+        generateCup(rutaCup);//activador//
+        //ejercicioParser1("C://Users//Usuario//Desktop//pru.txt");
+        //ejercicioLexer1("C://Users//Usuario//Desktop//pru.txt");        
+//generarLexer(rutaLexerCup);
         } catch (Exception ex) {
             Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
     }
     
     public static void generarLexer(String ruta) throws SilentExit {
@@ -38,4 +49,32 @@ public class Principal {
         String[] archivo = {"-parser", "Sintax", ruta};
         java_cup.Main.main(archivo);
     }
+//
+    
+public static void ejercicioLexer1(String rutaScanear) throws IOException{
+        Reader reader = new BufferedReader(new FileReader (rutaScanear));
+        reader.read();
+        LexerCup lex = new LexerCup(reader);
+        int i = 0;
+        Symbol token;
+        while(true)
+        {
+            token = lex.next_token();
+            if(token.sym != 0){
+                System.out.println("Token: "+token.sym+ ", Valor: "+(token.value==null?lex.yytext():token.value.toString()));
+            }
+            else{        
+                System.out.println("Cantidad de lexemas encontrados: "+i);
+                return;
+            }
+            i++;
+        }
+    }    
+//
+    public static void ejercicioParser1 (String rutaparsear) throws Exception{ 
+            Reader inputLexer = new FileReader (rutaparsear);
+            LexerCup myLexer = new LexerCup (inputLexer);
+            Sintax myParser = new Sintax ( myLexer);
+            myParser.parse();
+        }
 }
