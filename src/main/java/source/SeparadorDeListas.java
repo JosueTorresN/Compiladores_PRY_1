0e236bef10bcc76b13de5e2d281a3bd8811bbca3
DESKTOP-0SDNMEI\Usuario <anthony00052023@gmail.com>
2023-11-20 19:28:18 -0600

    commit CHANGES

---
+++ src/main/java/source/SeparadorDeListas.java
@@ -0,0 +1,239 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package source;
+
+/**
+ *
+ * @author Usuario
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SeparadorDeListas {
+    private boolean[] registrosT = new  boolean[10];
+    private static List<String> generatedLines = new ArrayList<>();
+    private static String[] lista = {"variable_globalTs_", "variable_int_", "variable_forEach_main_","variable_main_"};
+    private static String reserOperaciones = "\\^%+\\-\\*&/";
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\Compiladores_PRY_1\\3D_Code.txt"; // Reemplaza con la ruta de tu archivo
+
+        try {
+            List<List<String>> listOfLists = readAndProcessFile(filePath);
+
+            // Imprimir cada elemento de cada lista
+            for (List<String> list : listOfLists) {
+                convertToAssembler(list);//LLamada a la funcion que escribe el asm
+                printList(list);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.asm"))) {
+            for (String asmLine : generatedLines) {
+                writer.write(asmLine);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //función encargada de pasar el contenido a mips
+    // Lee el archivo y procesa sus líneas en listas
+    public static List<List<String>> readAndProcessFile(String filePath) throws IOException {
+        List<List<String>> listOfLists = new ArrayList<>();
+        List<String> currentList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            boolean flag = false;
+            while ((line = br.readLine()) != null) {
+
+                if (line.contains(":")&& flag == false) {
+                    if (!currentList.isEmpty()) {
+                        listOfLists.add(new ArrayList<>(currentList));
+                        currentList.clear();
+                    }
+                }
+                if (line.startsWith("DATA:")){
+                    flag = true;
+                }
+                currentList.add(line);
+            }
+            
+        }
+
+        // Añadir la última lista si no está vacía
+        if (!currentList.isEmpty()) {
+            listOfLists.add(new ArrayList<>(currentList));
+        }
+
+        return listOfLists;
+    }
+
+    // Imprime los elementos de una lista
+    public static void printList(List<String> list) {
+        for (String element : list) {
+            //System.out.println(element);
+        }
+        //System.out.println("---- Fin de la lista ----");
+    }
+    
+    
+    //clase encargada de convertir en ensamblador
+    private static int tempCounter = 0;
+
+    public static void convertToAssembler(List<String> inputLines) {
+        //generatedLines.clear();
+
+        //generatedLines.add(inputLines.get(0));
+        generatedLines.add(".data");
+        for (int i = 1; i < inputLines.size(); i++) {
+            String line = inputLines.get(i);
+            
+            //System.out.println("Lado izquierdo "+line);
+            if (line.contains("=") && !compararConLista(line)) {//Si tiene igual y si no es una variable declarada en el data entonces ingresa
+                //System.out.println("respuesta de la comparacion "+compararConLista(line));
+                String[] parts = line.split("=");
+                String leftHandSide = parts[0].trim();
+                String rightHandSide = parts[1].trim();
+                //System.out.println("Elemento "+rightHandSide);
+                //
+                boolean assignedToTemp = leftHandSide.matches("t[0-9]+");
+                if(!assignedToTemp){
+                    generatedLines.add("sw $" + rightHandSide + ", " + leftHandSide);
+                }
+                
+                //
+                if (rightHandSide.startsWith("-")) {
+                    // Es un número negativo
+                    String dest = leftHandSide;
+                    String value = rightHandSide.substring(1).trim(); // Eliminar el signo negativo
+                    generatedLines.add("li $" + dest + ", -" + value);
+                } else if (rightHandSide.matches(".*["+reserOperaciones+"].*")) {
+                    // Hay una operación aritmética
+                    rightHandSide = rightHandSide.replace("||", "|");
+                    rightHandSide = rightHandSide.replace("&&", "&");
+                    String[] operationParts = rightHandSide.split("["+reserOperaciones+"]");
+                    String dest = leftHandSide;
+                    String src1 = operationParts[0].trim();
+                    //System.out.println("lado izq "+leftHandSide+" src1 "+src1);
+                    String src2 = operationParts[1].trim();                    
+                    String operator = rightHandSide.replaceAll("[^"+reserOperaciones+"]", "");
+                    
+                    // Reemplazar los símbolos
+                    switch (operator) {
+                        case "+":
+                            operator = "add";
+                            break;
+                        case "-":
+                            operator = "addi"; // Sumar con el valor negativo
+                            src2 = "-" + src2;
+                            break;
+                        case "*":
+                            operator = "mul";
+                            break;
+                        case "/":
+                            operator = "div";
+                            break;
+                        case "%":
+                            operator = "div";
+                            break; 
+                        case "^":
+                            operator = "pot";
+                            break;
+                        case "&":
+                            operator = "and";
+                            break;
+                        case "|":
+                            operator = "or";
+                            break;     
+                        // Puedes agregar casos para otros operadores aquí
+                    }
+                    
+                    if (!operator.equals("pot")){
+                    generatedLines.add(operator + " $" + dest + ", $" + src1 + ", $" + src2);
+                    }else{
+                        creadorPotencias(src1,src2);
+                       
+                    }
+                    //Agragar un if para guardar el cocioente si operador == div
+                } else {
+                    // Asignación simple
+                    String dest = leftHandSide;
+                    String value = rightHandSide.trim();
+                    generatedLines.add("li $" + dest + ", " + value);
+                }
+            }else{
+                if(line.startsWith("begin_Func_globalTs:")){
+                    generatedLines.add(".text");
+                }else if(line.contains(":")){
+                    generatedLines.add(line);
+                }else{
+                    
+                    generatedLines.add(" . "+splitter(line));
+                }
+            }
+        }
+    }
+    
+    //Funcion encargada de compara el valor proveniente del metodo convertToAssembler y 
+    public static boolean compararConLista(String str) {
+        // Lista de valores predefinidos
+        //System.out.println("compara eso => "+str);
+ 
+        // Iterar sobre la lista y comparar con el String dado
+        for (String valor : lista) {
+            //System.out.println("Valor que ingresa"+ str);
+            //System.out.println("valor de esto => "+valor.startsWith(str));
+            if (str.startsWith(valor)) {
+                return true; // Coincidencia encontrada
+            }
+        }
+
+        return false; // No se encontró coincidencia
+    }
+    
+    //Funcion encargada de compara el valor proveniente del metodo convertToAssembler y 
+    public static String splitter(String str) {
+        // Lista de valores predefinidos
+
+        //System.out.println("soy un string "+str);
+        // Iterar sobre la lista y comparar con el String dado
+        for (int i = 0; i < lista.length; i++) {
+            if (str.startsWith(lista[i])) {
+                String nVar = str.replace(lista[i],"");
+                ///Seccion donde se splitea
+                String [] nVarSplited = nVar.split("=");
+                nVar = nVarSplited[0];
+                //System.out.println("Me splitee "+nVar);
+                ///Seccion donde se splitea
+                return nVar; // Coincidencia encontrada
+            }
+        }
+
+         // No se encontró coincidencia
+        return null;
+    }
+    
+    public static void creadorPotencias(String src1,String src2){
+        System.out.println("Hay");
+        generatedLines.add("move $t0, "+src1);
+        generatedLines.add("bucle_potencia:");
+        generatedLines.add("  beqz"+ "$"+src1+", fin_bucle");
+        generatedLines.add("  mul "+ "$" + src1 +", $"+ src1 +", $t0" );
+        generatedLines.add("  subi "+ "$" + src2 +", $"+ src2 +", 1");
+        generatedLines.add("  j bucle_potencia");
+        generatedLines.add("fin_bucle:");
+        generatedLines.add("  jr ra");
+    }
+    
+}
